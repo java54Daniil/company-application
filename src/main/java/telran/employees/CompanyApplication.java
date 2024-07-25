36608496a5cb283a07f93ac7ff7d3a59b193f98c
Daniillevinsky <draconliveda@gmail.com>
2024-07-25 18:41:37 +0300

    HW #39 submenu for adding different Employee types

--- src/main/java/telran/employees/CompanyApplication.java
+++ src/main/java/telran/employees/CompanyApplication.java
@@ -19,8 +19,10 @@ public class CompanyApplication {
 		List<Item> companyItems =
 				CompanyApplItems.getCompanyItems(company,
 						new HashSet<String>(List.of("Audit", "Development", "QA")));
-		companyItems.add(Item.of("Exit & save",
+		companyItems.add(
+				Item.of("Exit & save",
 				io -> ((Persistable)company).save(FILE_NAME), true));
+		companyItems.add(Item.ofExit());
 		Menu menu = new Menu("Company CLI Application",
 				companyItems.toArray(Item[]::new));
 		menu.perform(new SystemInputOutput());
